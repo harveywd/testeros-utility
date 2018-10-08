e45dfd3d8184f8cbf739b21dad02a56a2c917499
Harvey <harvey@support.webartdesign.com.au>
2018-10-08 19:23:44 +0800

    remove unnecessary files

--- rules.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Available placeholders: Harveywd, TesterosUtility, harveywd, testerosutility
-return [
-    'src/MyPackage.php' => 'src/TesterosUtility.php',
-    'config/mypackage.php' => 'config/testerosutility.php',
-    'src/Facades/MyPackage.php' => 'src/Facades/TesterosUtility.php',
-    'src/MyPackageServiceProvider.php' => 'src/TesterosUtilityServiceProvider.php',
-];
